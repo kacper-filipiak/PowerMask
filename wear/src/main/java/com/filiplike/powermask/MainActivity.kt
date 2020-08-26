347e7ab53fb7dffbf458b75c9e7f627f3689b740
Kacper <kisielWrosole@gmail.com>
2020-08-26 15:43:07 +0200

    Implemented working in background as service

--- wear/src/main/java/com/filiplike/powermask/MainActivity.kt
+++ wear/src/main/java/com/filiplike/powermask/MainActivity.kt
@@ -28,31 +28,13 @@ class MainActivity : WearableActivity() {
     private var maskOn = false
     private var lockMedia = false
 
-    private lateinit var sensorMenager : SensorManager
-    private lateinit var rotationSensor: Sensor
+//    lateinit var sensorMenager : SensorManager
+//    lateinit var rotationSensor: Sensor
 
-    private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var vibrator: Vibrator
+    lateinit var mediaPlayer: MediaPlayer
+     lateinit var vibrator: Vibrator
 
     //adding listener for sensor state change
-    private val mLightSensorListener = object : SensorEventListener {
-        override fun onSensorChanged(event: SensorEvent) {
-            currentState = event.values
-            //if protection is activated
-            if (maskOn) {
-                //if occurred face touch
-                if (contectDetector.isContact(currentState)){
-                    makeBeep()
-                }else{
-                    //release media player
-                    lockMedia = false
-                }
-            }
-        }
-
-        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-        }
-    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,39 +42,39 @@ class MainActivity : WearableActivity() {
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener { maskWear() }
-
-        this.sensorMenager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        rotationSensor = sensorMenager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
-
+//        this.sensorMenager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        rotationSensor = sensorMenager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+//
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         mediaPlayer = MediaPlayer.create(this, R.raw.sound)
-
 
         // Enables Always-on
         setAmbientEnabled()
     }
     //handles beeping and buzzing
-    private fun makeBeep(){
-        //if media player unlocked
-        if (!lockMedia){
-            //lock media
-            lockMedia = true
-            //Vibrate
-            vibrator.vibrate(VibrationEffect.createOneShot(200,VibrationEffect.DEFAULT_AMPLITUDE))
-            //Play sound
-            mediaPlayer.start()
-            //add to send list
-            report.addItem(LocalDateTime.now())
-            //updates counter
-            counter++
-        }
-    }
+//    private fun makeBeep(){
+//        //if media player unlocked
+//        if (!lockMedia){
+//            //lock media
+//            lockMedia = true
+//            //Vibrate
+//            vibrator.vibrate(VibrationEffect.createOneShot(200,VibrationEffect.DEFAULT_AMPLITUDE))
+//            //Play sound
+//            mediaPlayer.start()
+//            //add to send list
+//            report.addItem(LocalDateTime.now())
+//            //updates counter
+//            counter++
+//        }
+//    }
+
     private fun maskWear(){
         if (!maskOn){
             //put on mask and recalibrating states
             contectDetector = ContactDetector(currentState)
             //enable notification via buzzing
             vibrator.vibrate(VibrationEffect.createOneShot(200,VibrationEffect.DEFAULT_AMPLITUDE))
+            mediaPlayer.start()
             //update UI
             button1.setText(R.string.button_on_text)
             imageView2.setImageResource(R.drawable.mask_on)
@@ -116,13 +98,12 @@ class MainActivity : WearableActivity() {
 
     override fun onResume() {
         super.onResume()
-        sensorMenager.registerListener(mLightSensorListener, rotationSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        //sensorMenager.registerListener(mLightSensorListener, rotationSensor, SensorManager.SENSOR_DELAY_NORMAL)
 
         }
     override fun onPause() {
         super.onPause()
-        sensorMenager.unregisterListener(mLightSensorListener)
-        mediaPlayer.release()
+        //mediaPlayer.release()
     }
 
 }
--- wear/src/main/java/com/filiplike/powermask/TrackingService.kt
+++ wear/src/main/java/com/filiplike/powermask/TrackingService.kt
@@ -6,15 +6,21 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.media.MediaPlayer
 import android.os.Build
 import android.os.IBinder
+import android.os.Vibrator
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class TrackingService : Service() {
 
-    private val CHANNEL_ID = "ForegroundService Kotlin"
+    private val CHANNEL_ID = "PowerMaskService"
+
 
     companion object {
         fun startService(context: Context, message: String) {
@@ -31,7 +37,12 @@ class TrackingService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         //do heavy work on a background thread
-        val input = intent?.getStringExtra("inputExtra")
+        val context = this
+        runBlocking { launch {
+            val maskOnMenager : MaskOnMenager = MaskOnMenager(context)
+        } }
+
+        val input = intent?.getStringExtra ("inputExtra")
         createNotificationChannel()
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(
@@ -46,11 +57,7 @@ class TrackingService : Service() {
             .build()
         startForeground(1, notification)
         //stopSelf();
-        constantSound()
         return START_NOT_STICKY
-    }
-    fun constantSound(){
-        print("here code")
     }
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -59,7 +66,7 @@ class TrackingService : Service() {
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val serviceChannel = NotificationChannel(
-                CHANNEL_ID, "PowerMask service",
+                CHANNEL_ID, "PowerMaskService",
                 NotificationManager.IMPORTANCE_DEFAULT
             )
             val manager = getSystemService(NotificationManager::class.java)
